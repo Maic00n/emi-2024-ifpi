f1b42bb73c943920b40bb988a4996cb915605937
Maic00n <rodriguesmaicon47322@gmail.com>
2025-10-30 14:12:08 -0300

    a

--- apps/aluno/views.py
+++ apps/aluno/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'aluno/index.html')
+
+def cadastro(request):
+    return render(request, 'aluno/cadastro.html')
